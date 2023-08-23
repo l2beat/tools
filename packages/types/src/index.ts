0adcb16d0a9c0922eeb908ea752161b8d9965650
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2023-08-23 11:30:45 +0200

    Rename types package to discovery-types and remove unneeded functions.

--- packages/types/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Discovery'
-export * from './EthereumAddress'
-export * from './Hash256'
-export * from './proxyDetails'
--- packages/types/src/utils/randomHexDigit.ts
+++
@@ -1,3 +0,0 @@
-export function randomHexDigit(): string {
-  return '0123456789abcdef'[Math.floor(Math.random() * 16)] ?? '0'
-}
