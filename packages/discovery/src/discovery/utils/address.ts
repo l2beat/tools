77a178a54a6edb10867cce8e580588243956a778
Mateusz Radomski <radomski.main@protonmail.com>
2023-12-01 11:16:21 +0100

    Create a FunctionSelectorDecoder with tests

--- packages/discovery/src/discovery/utils/address.ts
+++ packages/discovery/src/discovery/utils/address.ts
@@ -4,3 +4,7 @@ import { EthereumAddress } from '../../utils/EthereumAddress'
 export function bytes32ToAddress(bytes32: Bytes): EthereumAddress {
   return EthereumAddress(bytes32.slice(12, 32).toString())
 }
+
+export function addressToBytes32(address: EthereumAddress): Bytes {
+  return Bytes.fromHex(`000000000000000000000000${address.toString().slice(2)}`)
+}
